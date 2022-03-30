b01bf4b69c157bd798af9ed5f106eccd19ac96a1
Dplepler <david.plepler@gmail.com>
2022-03-30 17:50:55 +0300

    Add IRequestHandler

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -1,5 +1,37 @@
 #pragma once
+#include <vector>
+#include <ctime>
+
+typedef std::vector<char> Buffer;
+typedef unsigned int RequestId;
+
+typedef struct REQUEST_RESULT_STRUCT {
+
+	Buffer response;
+	IRequestHandler* newHandler;
+
+} RequestResult;
+
+typedef struct REQUEST_INFO_STRUCT {
+
+	RequestId id;
+	time_t time;
+	Buffer buffer;
+
+} RequestInfo;
+
 class IRequestHandler
 {
+
+public:
+
+	IRequestHandler() = default;
+
+	virtual bool isRequestRelevant(RequestInfo info) const = 0;
+	virtual RequestResult handleRequest(RequestInfo info) = 0;
+
+
 };
+
+
 
