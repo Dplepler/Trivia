b678882577121c0288275869036d6bd5348b4de0
Dplepler <david.plepler@gmail.com>
2022-03-30 17:35:55 +0300

    Add responses

---
+++ Trivia/Response.h
@@ -0,0 +1,13 @@
+#pragma once
+
+typedef struct LOGIN_RESPONSE_STRUCT {
+
+	unsigned int status;
+
+} LoginResponse;
+
+typedef struct SIGNUP_RESPONSE_STRUCT {
+
+	unsigned int status;
+
+} SignupResponse;
