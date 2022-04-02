a7d20eff5387b0903fa4632d5ad44994c7a4e94d
Bachuchee <gal.aloni@outlook.com>
2022-04-02 14:39:27 +0300

    Polished up the code, ready for MR

--- Client/TestClient.py
+++ Client/TestClient.py
@@ -1,5 +1,7 @@
 import socket
 
+
+# config
 IP_ADDR = '127.0.0.1'
 PORT = 8008
 
@@ -18,9 +20,12 @@ def main():
         return
 
 
+    # try/except for cases where the connection closes in the middle of the conversation
     try:
         data = sock.recv(1024).decode()
 
+        # right now the protocol isn't supported, so instead the client only reads five bytes from the server 
+        # and then returns Hello to the server if he happens to get Hello from the server as well.
         print("Incoming message: {}".format(data))
 
         if(data == 'HELLO'):
