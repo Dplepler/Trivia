85632f35b167ef3a532fdb1e38860df3784a9673
Bachuchee <gal.aloni@outlook.com>
2022-04-09 20:26:59 +0300

    Finish base code for expansion, needs to polish up and go over to see that everything works as expected

--- Client/TestClient.py
+++ Client/TestClient.py
@@ -1,11 +1,32 @@
 import socket
-
+import json
 
 
 CHUNK_SIZE = 1024
 # config
 IP_ADDR = '127.0.0.1'
 PORT = 8008
+
+# msg codes
+LOGIN_CODE = 0
+SIGNUP_CODE = 1
+
+
+
+def createLoginData():
+    username = input("Please enter username: ")
+    password = input("Please enter password: ")
+    data = {"username" : username, "password" : password}
+    return json.dumps(data)
+
+
+
+def createSignupData():
+    username = input("Please enter username: ")
+    password = input("Please enter password: ")
+    email = input("Please enter email address: ")
+    data = {"username" : username, "password" : password, "mail" : email}
+    return json.dumps(data)
 
 
 
@@ -22,16 +43,36 @@ def main():
         return
 
 
+    print("Please enter wether to send login msg or signup msg:")
+    print("login - 1")
+    print("signup - 2")
+    option = int(input("Enter option here: "))
+    while(option != 1) and (option != 2):
+        print("Invalid option!\nplease enter option again")
+        option = input("Enter option here: ")
+
+
+
+    if option == 1:
+        jsonData = createLoginData()
+        code = LOGIN_CODE
+    
+    else:
+        jsonData = createSignupData()
+        code = SIGNUP_CODE
+
+    
+    msgLen = len(jsonData)
+    print(msgLen)
+
+
+    fullMsg = (code).to_bytes(1, byteorder = "big") + (msgLen).to_bytes(4, byteorder = "big") + bytes(jsonData, 'utf-8')
+
     # try/except for cases where the connection closes in the middle of the conversation
     try:
-        data = sock.recv(CHUNK_SIZE).decode()
+        
+        sock.send(fullMsg)
 
-        # right now the protocol isn't supported, so instead the client only reads five bytes from the server 
-        # and then returns Hello to the server if he happens to get Hello from the server as well.
-        print("Incoming message: {}".format(data))
-
-        if(data == 'HELLO'):
-            sock.send('HELLO'.encode())
     
     except Exception:
         print("Connection with socket suddenly closed")
