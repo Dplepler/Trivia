b99b68315a122f290133ec5e8bb69c878584d5f9
Bachuchee <gal.aloni@outlook.com>
2022-04-16 17:23:06 +0300

    Finish LoginManager, needs testing and polishing and should be ready

--- Server/Trivia/LoginManager.cpp
+++ Server/Trivia/LoginManager.cpp
@@ -5,6 +5,7 @@
 LoginManager::LoginManager(IDatabase* db): m_database(db) { }
 
 
+
 bool LoginManager::signup(std::string username, std::string password, std::string email) {
 	if (m_database->doesUserExist(username) || username.empty() || password.empty() || email.empty()) { return false; }
 	m_database->addNewUser(username, password, email);
@@ -22,4 +23,13 @@ bool LoginManager::login(std::string username, std::string password) {
 }
 
 
-void logout(std::string username);
+void LoginManager::logout(std::string username) {
+	bool foundUser = false;
+	if (!m_database->doesUserExist(username)) { return; }
+	for (int i = 0; i < m_loggedUsers.size() && !foundUser; i++) {
+		if(m_loggedUsers[i].getUsername() == username) {
+			m_loggedUsers.erase(m_loggedUsers.begin() + i);
+			foundUser = true;
+		}
+	}
+}
