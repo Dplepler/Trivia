4e0d1dcab5d9c5cae27553269edfa4b16d3d8cab
Bachuchee <gal.aloni@outlook.com>
2022-04-27 12:44:57 +0300

    Create Android project for the trivia

---
+++ Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.trivia_android.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(28.dp),
+    medium = RoundedCornerShape(24.dp),
+    large = RoundedCornerShape(20.dp)
+)
