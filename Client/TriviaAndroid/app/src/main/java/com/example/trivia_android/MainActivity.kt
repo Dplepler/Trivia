013cca3ec0c4204d522fb2af1a6781adbb0fe623
Bachuchee <gal.aloni@outlook.com>
2022-04-27 18:33:38 +0300

    Fully set up app for trivia

--- Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/MainActivity.kt
+++ Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/MainActivity.kt
@@ -29,10 +29,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+
 
 @Preview(showBackground = true)
 @Composable
