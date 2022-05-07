56b867a859284b4de6153b36941770f4d8964296
Dplepler <david.plepler@gmail.com>
2022-05-07 22:44:03 +0300

    Get user statistics

--- Server/Trivia/StatisticsManager.cpp
+++ Server/Trivia/StatisticsManager.cpp
@@ -3,6 +3,18 @@
 StatisticsManager::StatisticsManager(IDatabase* database) : m_database(database) { }
 
 
-std::vector<std::string> StatisticsManager::getHighScore() { return std::vector<std::string>(); }
+std::vector<std::string> StatisticsManager::getHighScore() { 
 
-std::vector<std::string> getUserStatistics(std::string username) { return std::vector<std::string>(); }
+	return std::vector<std::string>();
+}
+
+std::vector<std::string> StatisticsManager::getUserStatistics(std::string username) {
+
+	return std::vector<std::string>
+		{
+		std::to_string(this->m_database->getNumOfCorrectAnswers(username)),
+		std::to_string(this->m_database->getNumOfPlayerGames(username)), 
+		std::to_string(this->m_database->getNumOfTotalAnswers(username)),
+		std::to_string(this->m_database->getPlayerAverageAnswerTime(username))
+		};
+}
