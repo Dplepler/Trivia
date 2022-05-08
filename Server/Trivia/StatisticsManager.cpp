feeadb76246bd13e2972f0e084d750479962cf99
Dplepler <david.plepler@gmail.com>
2022-05-08 13:56:07 +0300

    Finish integrating db and statistics manager

--- Server/Trivia/StatisticsManager.cpp
+++ Server/Trivia/StatisticsManager.cpp
@@ -2,10 +2,42 @@
 
 StatisticsManager::StatisticsManager(IDatabase* database) : m_database(database) { }
 
+std::string StatisticsManager::getHighestScoreName() {
+	
+	unsigned int maxScore = 0;
+	unsigned int score = 0;
+	std::string username = "";
+	std::vector<std::string> usernames = this->m_database->getAllUsernames();
+	size_t usernameAmount = usernames.size();
+
+	for (unsigned int i = 0; i < usernameAmount; i++) {
+		score = SCORE(usernames[i]);
+		if (score > maxScore) {
+			maxScore = score;
+			username = usernames[i];
+		}	
+	}
+	
+	return username;
+}
 
 std::vector<std::string> StatisticsManager::getHighScore() { 
 
-	return std::vector<std::string>();
+	std::string username;
+	std::vector<std::string> topPlayers;
+
+	std::vector<std::string> allUsernames = this->m_database->getAllUsernames();;
+
+	for (uint8_t i = 0; i < TOP_PLAYERS_AMOUNT; i++) {
+		username = this->getHighestScoreName();
+		if (username == "") { break; }
+		topPlayers.push_back(username);
+
+		std::vector<std::string>::iterator it = std::find(allUsernames.begin(), allUsernames.end(), username);
+		if (it != allUsernames.end()) { allUsernames.erase(it); }
+	}
+
+	return topPlayers;
 }
 
 std::vector<std::string> StatisticsManager::getUserStatistics(std::string username) {
--- Server/Trivia/StatisticsManager.h
+++ Server/Trivia/StatisticsManager.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "SqliteDatabase.h"
 
+#define TOP_PLAYERS_AMOUNT 5
+
+#define SCORE(name) ((m_database->getNumOfCorrectAnswers(name) / m_database->getNumOfTotalAnswers(name)) * 10000 / m_database->getPlayerAverageAnswerTime(name))
+
 class StatisticsManager {
 
 public:
@@ -14,5 +18,7 @@ public:
 private:
 
 	IDatabase* m_database;
+
+	std::string getHighestScoreName();
 
 };
