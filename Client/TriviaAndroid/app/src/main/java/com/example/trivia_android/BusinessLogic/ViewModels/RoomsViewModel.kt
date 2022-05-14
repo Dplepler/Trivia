b893b5913d8c3bd76bf8dbb7c30fffbaa85943d5
Bachuchee <gal.aloni@outlook.com>
2022-05-14 20:16:04 +0300

    Implement Info Dropdowns

---
+++ Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/BusinessLogic/ViewModels/RoomsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.trivia_android.BusinessLogic.ViewModels
+
+import androidx.lifecycle.ViewModel
+
+class RoomsViewModel: ViewModel() {
+
+}
---
+++ Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/ui/screens/Rooms/CreateRoom.kt
@@ -0,0 +1,112 @@
+package com.example.trivia_android.ui.screens.Rooms
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
+import com.example.trivia_android.ui.SharedComponents.SharpTextField
+import com.example.trivia_android.ui.theme.TriviaAndroidTheme
+
+
+@Composable
+fun InfoDropDown(
+    modifier: Modifier = Modifier,
+    label: String,
+    chosenValue: MutableState<String>,
+    options: List<Int>
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    var textfieldSize by remember { mutableStateOf(Size.Zero)}
+
+
+    val icon = if (expanded)
+        Icons.Filled.ArrowDropUp
+    else
+        Icons.Filled.ArrowDropDown
+
+
+    Column(modifier) {
+        OutlinedTextField(
+            modifier = modifier
+                .width(130.dp)
+                .onGloballyPositioned { coordinates ->
+                    textfieldSize = coordinates.size.toSize()
+                },
+            shape = RoundedCornerShape(8.dp),
+            value = chosenValue.value,
+            onValueChange = { chosenValue.value = it },
+            label = { Text(label) },
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            }
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textfieldSize.width.toDp() })
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(onClick = {
+                    chosenValue.value = option.toString()
+                    expanded = false
+                }) {
+                    Text(option.toString())
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+@Preview
+fun InfoPreview() {
+
+    val value = remember { mutableStateOf("") }
+    val value2 = remember { mutableStateOf("")}
+
+    TriviaAndroidTheme {
+
+        Row {
+
+            InfoDropDown(
+                modifier = Modifier.padding(end = 8.dp),
+                label = "Players",
+                chosenValue = value,
+                options = (1..30).toList()
+            )
+
+
+            InfoDropDown(
+                modifier = Modifier,
+                label = "Time",
+                chosenValue = value2,
+                options = (10..180 step(10)).toList()
+            )
+
+
+        }
+
+    }
+
+}
