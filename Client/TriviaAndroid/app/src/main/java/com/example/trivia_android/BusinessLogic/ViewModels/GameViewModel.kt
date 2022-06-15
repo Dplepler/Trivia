3dac967b9a8e3d383eeb4b48ab24ecaeb0754f45
Bachuchee <gal.aloni@outlook.com>
2022-06-15 14:21:31 +0300

    Finish creating server functions, should work

--- Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/BusinessLogic/ViewModels/GameViewModel.kt
+++ Client/TriviaAndroid/app/src/main/java/com/example/trivia_android/BusinessLogic/ViewModels/GameViewModel.kt
@@ -1,11 +1,10 @@
 package com.example.trivia_android.BusinessLogic.ViewModels
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.trivia_android.BusinessLogic.Communications.Communications
-import com.example.trivia_android.BusinessLogic.Communications.RequestCodes
-import com.example.trivia_android.BusinessLogic.Communications.ResponseCodes
+import com.example.trivia_android.BusinessLogic.Communications.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
@@ -33,9 +32,32 @@ data class CorrectAnswer (
 data class AnswerSubmition(val answerId: Int)
 
 
+@Serializable
+data class UserResults(
+    val username: String,
+    val averageTime: Float,
+    val correctAns: Int,
+    val wrongAns: Int
+)
+
+
+
+@Serializable
+data class GameResults (
+    val status: Int,
+    val results: List<UserResults>
+)
+
+
+data class UserScore(val username: String, val score: Int)
+
+
+
 class GameViewModel: ViewModel() {
 
     val comms = Communications
+
+    val userInfo = UserInfo
 
     val gameInfo = GameInfo
 
@@ -48,6 +70,14 @@ class GameViewModel: ViewModel() {
     val questionNumber = mutableStateOf( 0 )
 
     val curTime = mutableStateOf( 0 )
+
+    val userScores = mutableStateListOf<UserScore>()
+
+    val userStats = mutableStateOf(UserResults(" ", 0f, 0, 0))
+
+    companion object {
+        const val SCORE_MULTIPLIER = 10000
+    }
 
 
     fun getQuestion() {
@@ -81,10 +111,57 @@ class GameViewModel: ViewModel() {
 
 
 
+
+    fun getResults() {
+        viewModelScope.launch {
+            comms.sendMessage(comms.buildMessage(RequestCodes.GameResult.code.toByte(), ""))
+            val buffer = comms.readMessage()
+            if(buffer[0].toInt() == ResponseCodes.GetResults.code) {
+                val res = String(buffer).substring(comms.headerLen)
+                val resList = Json.decodeFromString<GameResults>(res).results
+                createScores(resList)
+            }
+        }
+    }
+
+
+
+    fun createScores(userResults: List<UserResults>) {
+
+        userScores.clear()
+
+        userResults.forEach {
+            if(it.username == userInfo.userName) { userStats.value = it }
+
+            userScores.add(UserScore(it.username, calcScore(it).toInt()))
+        }
+
+        userScores.sortBy { it.score }
+    }
+
+
+    fun calcScore(userResults: UserResults): Float =
+        if(userResults.averageTime == 0f) 0f
+        else userResults.correctAns / (userResults.correctAns + userResults.wrongAns) * SCORE_MULTIPLIER / userResults.averageTime
+
+
+
+    fun leaveGame(onSuccessLeave: () -> Unit) {
+        viewModelScope.launch {
+            comms.sendMessage(comms.buildMessage(RequestCodes.LeaveGame.code.toByte(), ""))
+            val buffer = comms.readMessage()
+            if(buffer[0].toInt() == ResponseCodes.LeaveGame.code) {
+                val res = String(buffer).substring(comms.headerLen)
+                if(Json.decodeFromString<Status>(res).status == 1) { onSuccessLeave }
+            }
+        }
+    }
+
+
     fun decreaseTime(onGameEnd: () -> Unit = { }) {
         curTime.value = (curTime.value - 1)
         if(curTime.value <= 0) {
-            if(questionNumber.value == gameInfo.questionCount) {
+            if(questionNumber.value >= gameInfo.questionCount) {
                 onGameEnd()
                 curTime.value = 0
             }
